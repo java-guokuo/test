fc8e3e76e1f51e6ee43958583776ab0848263de1
1035731496@qq.com <1035731496@qq.com>
2019-02-28 12:53:10 +0800

    qqqq

---
+++ gitinfo/src/main/java/net/gdface/facelog/service/Version.java
@@ -0,0 +1,11 @@
+package net.gdface.facelog.service;
+public final class Version {
+    /** project version */
+    public static final String VERSION = "${project.version}";
+    /** SCM(git) revision */
+    public static final String SCM_REVISION= "${buildNumber}";
+    /** SCM branch */
+    public static final String SCM_BRANCH = "${scmBranch}";
+    /** build timestamp */
+    public static final String TIMESTAMP ="${buildtimestamp}";
+}
